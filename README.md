a7236794c9f5b62643dd8caad0b3f0c877c05c9c
Florian Beaufumé <fbeaufume@gmail.com>
2019-03-06 11:04:38 +0100

    Added initial Spring Boot configuration

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# spring-graphql-sample
-Sample GraphQL backend with Spring Boot
+# Spring GraphQL Sample
+
+**Work in progress**
+
+This is a sample GraphQL backend based on Spring Boot.
+
+Run with `mvn spring-boot:run`.
---
+++ src/main/java/com/adeliosys/sample/GraphQLApplication.java
@@ -0,0 +1,12 @@
+package com.adeliosys.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLApplication.class, args);
+    }
+}
